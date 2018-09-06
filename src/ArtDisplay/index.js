5ddb5c22d4f17f1c91723fa91a6469a9464c8dbd
Torjus Iveland <torjusti@gmail.com>
2018-09-06 14:34:19 +0200

    Run Prettier on previous code

--- src/ArtDisplay/index.js
+++ src/ArtDisplay/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './ArtDisplay.css';
 
 const ArtDisplay = () => (
-  <div className="art-display">
-    Dynamic image thing here
-  </div>
+  <div className="art-display">Dynamic image thing here</div>
 );
 
 export default ArtDisplay;
--- src/ArtPicker/index.js
+++ src/ArtPicker/index.js
@@ -3,7 +3,7 @@ import Tab from './Tab';
 import './ArtPicker.css';
 
 const ArtPicker = () => (
-  <div className='art-picker'>
+  <div className="art-picker">
     <Tab name="Kunstverk 1" />
     <Tab name="Kunstverk 2" />
     <Tab name="Kunstverk 3" />
