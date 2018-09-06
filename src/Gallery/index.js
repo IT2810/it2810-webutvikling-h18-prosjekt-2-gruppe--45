9a65922d9257ff1aca5983bdf1411cfbbf0d828b
Torjus Iveland <torjusti@gmail.com>
2018-09-06 17:24:09 +0200

    Hoist MediaPicker state to Gallery

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MediaPicker from '../MediaPicker';
 import ArtPicker from '../ArtPicker';
 import ArtDisplay from '../ArtDisplay';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div className="gallery">
-    <MediaPicker />
-    <ArtPicker />
-    <ArtDisplay />
-  </div>
-);
+class Gallery extends Component {
+  /**
+   * States for the three formats of categories.
+   * @type {{image: string, audio: string, text: string}}
+   */
+  state = {
+    image: 'chicken',
+    audio: 'pigs',
+    text: 'flames',
+  };
+
+  handleImageChange = event => {
+    this.setState({
+      image: event.currentTarget.value,
+    });
+  };
+
+  handleAudioChange = event => {
+    this.setState({
+      audio: event.currentTarget.value,
+    });
+  };
+
+  handleTextChange = event => {
+    this.setState({
+      text: event.currentTarget.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="gallery">
+        <MediaPicker
+          onImageChange={this.handleImageChange}
+          onAudioChange={this.handleAudioChange}
+          onTextChange={this.handleTextChange}
+          selectedImage={this.state.image}
+          selectedAudio={this.state.audio}
+          selectedText={this.state.text}
+        />
+
+        <ArtPicker />
+
+        <ArtDisplay />
+      </div>
+    );
+  }
+}
 
 export default Gallery;
--- src/MediaPicker/index.js
+++ src/MediaPicker/index.js
@@ -4,154 +4,150 @@ import './MediaPicker.css';
 /**
  * Class for handling users mediapicks.
  */
-class MediaPicker extends React.Component {
+const MediaPicker = ({
+  onImageChange,
+  onAudioChange,
+  onTextChange,
+  selectedImage,
+  selectedAudio,
+  selectedText,
+}) => (
+  <div className="media-picker">
+    <form>
+      <div>
+        <h2>Bilder</h2>
 
-  constructor(props) {
-    super(props);
+        <div>
+          <ul>
+            <li>
+              <label>
+                <input
+                  name="image"
+                  type="radio"
+                  value="chicken"
+                  checked={selectedImage === 'chicken'}
+                  onChange={onImageChange}
+                />
+                Kyllinger
+              </label>
+            </li>
 
-    /**
-     * States for the three formats of categories
-     * @type {{image: string, audio: string, text: string}}
-     */
-    this.state = {
-      image: "chicken",
-      audio: "pigs",
-      text: "flames"
-    };
-  }
+            <li>
+              <label>
+                <input
+                  name="image"
+                  type="radio"
+                  value="flames"
+                  checked={selectedImage === 'flames'}
+                  onChange={onImageChange}
+                />
+                Flammer
+              </label>
+            </li>
 
-  /**
-   * Eventlisteners for the radiobuttons
-   * @param e   the pressed radiobutton
-   */
-  onImageChanged = (e) => {
-    this.setState({
-      image: (e.currentTarget.value)
-    });
-  };
+            <li>
+              <label>
+                <input
+                  name="image"
+                  type="radio"
+                  value="weather"
+                  checked={selectedImage === 'weather'}
+                  onChange={onImageChange}
+                />
+                Weather
+              </label>
+            </li>
+          </ul>
+        </div>
 
-  onAudioChanged = (e) => {
-    this.setState({
-      audio: e.currentTarget.value
-    });
-  };
+        <h2>Lyder</h2>
 
-  onTextChanged = (e) => {
-    this.setState({
-      text: e.currentTarget.value
-    });
-  };
+        <div>
+          <ul>
+            <li>
+              <label>
+                <input
+                  name="audio"
+                  type="radio"
+                  value="pigs"
+                  checked={selectedAudio === 'pigs'}
+                  onChange={onAudioChange}
+                />
+                Griser
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  name="audio"
+                  type="radio"
+                  value="flames"
+                  checked={selectedAudio === 'flames'}
+                  onChange={onAudioChange}
+                />
+                Flammer
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  name="audio"
+                  type="radio"
+                  value="weather"
+                  checked={selectedAudio === 'weather'}
+                  onChange={onAudioChange}
+                />
+                Vær
+              </label>
+            </li>
+          </ul>
+        </div>
 
+        <h2>Tekst</h2>
 
-  render() {
-    return (
-      <div className="media-picker">
-        <form>
-          <div>
-            <h2>Bilder</h2>
-            <div>
-              <ul>
-                <li>
-                  <label>
-                    <input name={"image"} type={"radio"} value={"chicken"}
-                           checked={this.state.image === "chicken"}
-                           onChange={this.onImageChanged}
-                    />
-                    Chickens
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"image"} type={"radio"} value={"flames"}
-                           checked={this.state.image === "flames"}
-                           onChange={this.onImageChanged}
-                    />
-                    Flames
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"image"} type={"radio"} value={"weather"}
-                           checked={this.state.image === "weather"}
-                           onChange={this.onImageChanged}
-                    />
-                    Weather
-                  </label>
-                </li>
-              </ul>
-            </div>
-
-            <h2>Lyder</h2>
-            <div>
-              <ul>
-                <li>
-                  <label>
-                    <input name={"audio"} type={"radio"} value={"pigs"}
-                           checked={this.state.audio === "pigs"}
-                           onChange={this.onAudioChanged}
-                    />
-                    Griser
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"audio"} type={"radio"} value={"flames"}
-                           checked={this.state.audio === "flames"}
-                           onChange={this.onAudioChanged}
-                    />
-                    Flammer
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"audio"} type={"radio"} value={"weather"}
-                           checked={this.state.audio === "weather"}
-                           onChange={this.onAudioChanged}
-                    />
-                    Weather
-                  </label>
-                </li>
-              </ul>
-            </div>
-
-            <h2>Tekst</h2>
-            <div>
-              <ul>
-                <li>
-                  <label>
-                    <input name={"text"} type={"radio"} value={"flames"}
-                           checked={this.state.text === "flames"}
-                           onChange={this.onTextChanged}
-                    />
-                    Brennende dikt
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"text"} type={"radio"} value={"jokes"}
-                           checked={this.state.text === "jokes"}
-                           onChange={this.onTextChanged}
-                    />
-                    Vitser
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input name={"text"} type={"radio"} value={"songs"}
-                           checked={this.state.text === "songs"}
-                           onChange={this.onTextChanged}
-                    />
-                    Sanger
-                  </label>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </form>
+        <div>
+          <ul>
+            <li>
+              <label>
+                <input
+                  name="text"
+                  type="radio"
+                  value="flames"
+                  checked={selectedText === 'flames'}
+                  onChange={onTextChange}
+                />
+                Brennende dikt
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  name="text"
+                  type="radio"
+                  value="jokes"
+                  checked={selectedText === 'jokes'}
+                  onChange={onTextChange}
+                />
+                Vitser
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  name="text"
+                  type="radio"
+                  value="songs"
+                  checked={selectedText === 'songs'}
+                  onChange={onTextChange}
+                />
+                Sanger
+              </label>
+            </li>
+          </ul>
+        </div>
       </div>
-    );
-  }
-}
-
+    </form>
+  </div>
+);
 
 export default MediaPicker;
