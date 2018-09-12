d981ec4e3c19465c7761b30850dc98a077176d2a
Hehug7 <tull-0@hotmail.com>
2018-09-12 11:41:53 +0200

    Adds global strings and randomized initial gallery values

--- src/MediaPicker/index.js
+++ src/MediaPicker/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import './MediaPicker.css';
 
+const CHICKEN_STRING = 'chicken';
+const PIGS_STRING = 'pigs';
+const FLAMES_STRING = 'flames';
+const WEATHER_STRING = 'weather';
+const JOKES_STRING = 'jokes';
+const SONGS_STRING = 'songs';
+
 /**
- * Class for handling users mediapicks.
+ * Class for handling users picking media category.
  */
 class MediaPicker extends React.Component {
-
   constructor(props) {
     super(props);
 
     /**
+     * Creates an array 'randomIndexes' that contains three random indexes
+     * between 0 and 2
+     */
+    let randomIndexes = [];
+
+    for (let i = 0; i < 3; i++) {
+      randomIndexes.push(Math.floor(Math.random() * 3));
+    }
+
+    /**
+     * Lists of strings for available images, audios and texts
+     */
+    const IMAGES = [CHICKEN_STRING, FLAMES_STRING, WEATHER_STRING];
+    const AUDIOS = [PIGS_STRING, FLAMES_STRING, WEATHER_STRING];
+    const TEXTS = [FLAMES_STRING, JOKES_STRING, SONGS_STRING];
+
+    /**
+     * Sets the chosen category based on the random indexes
      * States for the three formats of categories
      * @type {{image: string, audio: string, text: string}}
      */
     this.state = {
-      image: "chicken",
-      audio: "pigs",
-      text: "flames"
+      image: IMAGES[randomIndexes[0]],
+      audio: AUDIOS[randomIndexes[1]],
+      text: TEXTS[randomIndexes[2]],
     };
   }
 
@@ -24,24 +48,23 @@ class MediaPicker extends React.Component {
    * Eventlisteners for the radiobuttons
    * @param e   the pressed radiobutton
    */
-  onImageChanged = (e) => {
+  onImageChanged = e => {
     this.setState({
-      image: (e.currentTarget.value)
+      image: e.currentTarget.value,
     });
   };
 
-  onAudioChanged = (e) => {
+  onAudioChanged = e => {
     this.setState({
-      audio: e.currentTarget.value
+      audio: e.currentTarget.value,
     });
   };
 
-  onTextChanged = (e) => {
+  onTextChanged = e => {
     this.setState({
-      text: e.currentTarget.value
+      text: e.currentTarget.value,
     });
   };
-
 
   render() {
     return (
@@ -53,27 +76,36 @@ class MediaPicker extends React.Component {
               <ul>
                 <li>
                   <label>
-                    <input name={"image"} type={"radio"} value={"chicken"}
-                           checked={this.state.image === "chicken"}
-                           onChange={this.onImageChanged}
+                    <input
+                      name={'image'}
+                      type={'radio'}
+                      value={CHICKEN_STRING}
+                      checked={this.state.image === CHICKEN_STRING}
+                      onChange={this.onImageChanged}
                     />
                     Chickens
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"image"} type={"radio"} value={"flames"}
-                           checked={this.state.image === "flames"}
-                           onChange={this.onImageChanged}
+                    <input
+                      name={'image'}
+                      type={'radio'}
+                      value={FLAMES_STRING}
+                      checked={this.state.image === FLAMES_STRING}
+                      onChange={this.onImageChanged}
                     />
                     Flames
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"image"} type={"radio"} value={"weather"}
-                           checked={this.state.image === "weather"}
-                           onChange={this.onImageChanged}
+                    <input
+                      name={'image'}
+                      type={'radio'}
+                      value={WEATHER_STRING}
+                      checked={this.state.image === WEATHER_STRING}
+                      onChange={this.onImageChanged}
                     />
                     Weather
                   </label>
@@ -86,27 +118,36 @@ class MediaPicker extends React.Component {
               <ul>
                 <li>
                   <label>
-                    <input name={"audio"} type={"radio"} value={"pigs"}
-                           checked={this.state.audio === "pigs"}
-                           onChange={this.onAudioChanged}
+                    <input
+                      name={'audio'}
+                      type={'radio'}
+                      value={PIGS_STRING}
+                      checked={this.state.audio === PIGS_STRING}
+                      onChange={this.onAudioChanged}
                     />
                     Griser
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"audio"} type={"radio"} value={"flames"}
-                           checked={this.state.audio === "flames"}
-                           onChange={this.onAudioChanged}
+                    <input
+                      name={'audio'}
+                      type={'radio'}
+                      value={FLAMES_STRING}
+                      checked={this.state.audio === FLAMES_STRING}
+                      onChange={this.onAudioChanged}
                     />
                     Flammer
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"audio"} type={"radio"} value={"weather"}
-                           checked={this.state.audio === "weather"}
-                           onChange={this.onAudioChanged}
+                    <input
+                      name={'audio'}
+                      type={'radio'}
+                      value={WEATHER_STRING}
+                      checked={this.state.audio === WEATHER_STRING}
+                      onChange={this.onAudioChanged}
                     />
                     Weather
                   </label>
@@ -119,27 +160,36 @@ class MediaPicker extends React.Component {
               <ul>
                 <li>
                   <label>
-                    <input name={"text"} type={"radio"} value={"flames"}
-                           checked={this.state.text === "flames"}
-                           onChange={this.onTextChanged}
+                    <input
+                      name={'text'}
+                      type={'radio'}
+                      value={FLAMES_STRING}
+                      checked={this.state.text === FLAMES_STRING}
+                      onChange={this.onTextChanged}
                     />
                     Brennende dikt
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"text"} type={"radio"} value={"jokes"}
-                           checked={this.state.text === "jokes"}
-                           onChange={this.onTextChanged}
+                    <input
+                      name={'text'}
+                      type={'radio'}
+                      value={JOKES_STRING}
+                      checked={this.state.text === JOKES_STRING}
+                      onChange={this.onTextChanged}
                     />
                     Vitser
                   </label>
                 </li>
                 <li>
                   <label>
-                    <input name={"text"} type={"radio"} value={"songs"}
-                           checked={this.state.text === "songs"}
-                           onChange={this.onTextChanged}
+                    <input
+                      name={'text'}
+                      type={'radio'}
+                      value={SONGS_STRING}
+                      checked={this.state.text === SONGS_STRING}
+                      onChange={this.onTextChanged}
                     />
                     Sanger
                   </label>
@@ -152,6 +202,5 @@ class MediaPicker extends React.Component {
     );
   }
 }
-
 
 export default MediaPicker;
