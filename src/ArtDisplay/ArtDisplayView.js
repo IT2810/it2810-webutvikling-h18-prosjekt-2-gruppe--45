4827b1550aad1b78c1a66c082c6dbd90802e68dc
Hehug7 <tull-0@hotmail.com>
2018-09-13 15:45:33 +0200

    Color changes and fixes to css

--- src/ArtDisplay/ArtDisplayView.js
+++ src/ArtDisplay/ArtDisplayView.js
@@ -3,10 +3,11 @@ import React, { Fragment } from 'react';
 const ArtDisplay = ({ image, audio, text }) => (
   <Fragment>
     <div dangerouslySetInnerHTML={{ __html: image }} />
-
-    <h3>{text.title}</h3>
-    <p className="text">{text.text}</p>
-    <p>{text.author}</p>
+    <div>
+      <h3>{text.title}</h3>
+      <p className="text">{text.text}</p>
+      <p>{'~ ' + text.author}</p>
+    </div>
 
     <audio loop autoPlay>
       <source src={audio} type="audio/mpeg" />
